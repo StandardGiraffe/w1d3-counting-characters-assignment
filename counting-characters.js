dc5148859122adc1dd0d8ed104bc379c02e3c962
StandardGiraffe <dannyfekete@gmail.com>
2018-08-29 17:10:00 +0000

    Triumph!

--- counting-characters.js
+++ counting-characters.js
@@ -1,27 +1,21 @@
 // # w1d3-counting-characters-assignment
 // w1d3 Counting Characters Assignment
+
 var userArg = process.argv.slice(2).join("").toLowerCase();
-
-console.log(userArg);
-
-// ### Algorithm:
-
-// - Take input (argv) and remove spaces
-
-
-
-// + Build an empty object
-// - Travel through the processed input one character at a time
-//   - read the character at current index
-//   - check object to see if current character exists as a key in it
-//     - if true, increment
-//     - if false, create key with name of current character = 1
-// - Return the object
 
 var countCharacters = function (string) {
   var letterCount = {};
 
+  for (var i of string) {
+    if (!letterCount[i]) {
+      letterCount[i] = 0;
+    }
 
+    letterCount[i] += 1;
+
+  }
 
   return letterCount;
 }
+
+console.log(countCharacters(userArg));
